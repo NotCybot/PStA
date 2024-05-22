ba23c7a8222d69ee0b5c2d245378452160e66927
NotCybot <141260979+NotCybot@users.noreply.github.com>
2024-05-22 13:49:42 +0200

    added Stadt and Grossstadt

--- psta-src/src/main/java/th/rosenheim/oop/Grossstadt.java
+++ psta-src/src/main/java/th/rosenheim/oop/Grossstadt.java
@@ -1,4 +1,23 @@
 package th.rosenheim.oop;
 
-public class Grossstadt {
+import java.util.List;
+
+public class Grossstadt extends Stadt {
+    private List<String> stadtteile;
+
+    public Grossstadt(String name, Wetter wetter, List<String> stadtteile) {
+        super(name, wetter);
+        this.stadtteile = stadtteile;
+    }
+    @Override
+    public String getContent() {
+        String stadteileString = String.join(", ", stadtteile.subList(0, stadtteile.size() - 1)) + " und " + stadtteile.get(stadtteile.size() - 1);
+        return "<p>In " + name + "ist es " + wetter.toString().toLowerCase()+".<p>"+
+                "<p>Dies trifft auch für " + stadteileString + "zu.</p>";
+    }
+
+    @Override
+    public String getURL() {
+        return "wetter_grossstadt_" + formatURL() + ".html";
+    }
 }
--- psta-src/src/main/java/th/rosenheim/oop/Stadt.java
+++ psta-src/src/main/java/th/rosenheim/oop/Stadt.java
@@ -1,4 +1,23 @@
 package th.rosenheim.oop;
 
-public class Stadt {
+
+public abstract  class Stadt {
+    protected String name;
+    protected Wetter wetter;
+
+    public Stadt(String name, Wetter wetter) {
+        this.name = name;
+        this.wetter = wetter;
+    }
+
+    public abstract String getContent();
+
+    protected String formatURL() {
+        return  name.toLowerCase()
+                .replace(" ", "_")
+                .replace("ä", "ae")
+                .replace("ö", "oe")
+                .replace("ü", "ue");
+    }
+    public abstract String getURL();
 }
