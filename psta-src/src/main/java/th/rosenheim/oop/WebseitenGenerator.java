53e120505e37b29c634d6df761215844e74658a3
NotCybot <141260979+NotCybot@users.noreply.github.com>
2024-05-22 15:41:06 +0200

    added code for WebseitenGenerator

--- psta-src/src/main/java/th/rosenheim/oop/WebseitenGenerator.java
+++ psta-src/src/main/java/th/rosenheim/oop/WebseitenGenerator.java
@@ -1,4 +1,27 @@
 package th.rosenheim.oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WebseitenGenerator {
+    private List<Stadt>  staedte = new ArrayList<>();
+
+    public void addStadt(Stadt stadt) {
+        staedte.add(stadt);
+    }
+    public String getNavigator() {
+        String nav = "";
+        for (Stadt stadt : staedte) {
+            if (!nav.isEmpty()) {
+                nav += "| ";
+            }
+            nav += "<a href=\"" + stadt.getURL() + "\">" + stadt.name + "</a>";
+        }
+        return "<h1>Die Wetter-Website</h1><p>" + nav + "</p>";
+    }
+    private String generatePage(String navigation, String content) {
+        return "<html><body>"+navigation+content+"</body></html>";
+    }
+
+
 }
